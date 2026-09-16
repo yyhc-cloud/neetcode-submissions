1fb4ff003c4fe510df520563f9e26b9ec87d11b1
yyhc-cloud <yuhang981@gmail.com>
2026-09-16 13:53:01 +0800

    Add: anagram-groups - submission-5

---
+++ Data Structures & Algorithms/anagram-groups/submission-5.py
@@ -0,0 +1,15 @@
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        hashMap = {}
+
+        for item in strs:
+            key = "".join(sorted(item))
+
+            if key in hashMap:
+                hashMap[key].append(item)
+
+            else:
+                hashMap[key] = [item]
+
+        return list(hashMap.values())        
+
