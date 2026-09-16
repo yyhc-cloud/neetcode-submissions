d53456e028f72e0db4cac40525b7f18a93ffa101
yyhc-cloud <yuhang981@gmail.com>
2026-09-16 14:46:04 +0800

    Add: top-k-elements-in-list - submission-21

---
+++ Data Structures & Algorithms/top-k-elements-in-list/submission-21.py
@@ -0,0 +1,12 @@
+from typing import List
+from collections import Counter
+
+class Solution:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        count = Counter(nums)  # e.g., {1: 3, 2: 2, 3: 1}
+        
+        # Sort the unique keys by their frequency in reverse order
+        sorted_keys = sorted(count.keys(), key=lambda x: count[x], reverse=True)
+        
+        # Take the first k elements
+        return sorted_keys[:k]
